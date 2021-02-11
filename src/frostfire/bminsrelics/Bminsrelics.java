bdd38292e94a048ec895caf4eead161f686cfd0c
Ethane03 <ethan03@tjfmail.com>
2021-02-10 20:31:24 -0600

    More setup

--- src/frostfire/bminsrelics/Bminsrelics.java
+++ src/frostfire/bminsrelics/Bminsrelics.java
@@ -1,4 +1,17 @@
 package frostfire.bminsrelics;
 
-public class Bminsrelics{
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Bminsrelics extends JavaPlugin {
+    public String version = "0.0.1";
+    public String updatemsg = "";
+    @Override
+    public void onEnable() {
+        getLogger().info("Ethan and Brendon's epic SMP plugin has been enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Ethan and Brendon's epic SMP plugin has been disabled.");
+    }
 }
