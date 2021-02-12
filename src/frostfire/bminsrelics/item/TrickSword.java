347d1b841f49267883ef9b849acd3eb824b01306
Ethane03 <ethan03@tjfmail.com>
2021-02-11 19:09:21 -0600

    Swrod of godsw?

--- src/frostfire/bminsrelics/item/TrickSword.java
+++ src/frostfire/bminsrelics/item/TrickSword.java
@@ -1,10 +1,13 @@
 package frostfire.bminsrelics.item;
 
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.*;
+import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -12,20 +15,24 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 
 
-public class Infinidirt extends Relic {
+public class TrickSword extends Relic {
 
     @Override
     public Relic init() {
-        directory = "infinidirt";
-        ItemStack temp = new ItemStack(Material.STICK);
+        directory = "tricksword";
+        ItemStack temp = new ItemStack(Material.WOODEN_SWORD);
         ItemMeta meta = temp.getItemMeta();
-        meta.setDisplayName("§6Infinidirt");
+        meta.setDisplayName("§6God's toothpick");
         List<String> lore = new ArrayList<>();
-        lore.add("§3All the dirt you could ever want");
+        lore.add("§3Epicly powerful");
+        lore.add("§3Use it wisely");
         meta.setLore(lore);
-        meta.addEnchant(Enchantment.MENDING, 1, false);
+        meta.addEnchant(Enchantment.KNOCKBACK, 100, false);
+        AttributeModifier mod = new AttributeModifier(UUID.randomUUID(),"generic.attack_damage",10000.0, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND)
+        meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE,mod);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         temp.setItemMeta(meta);
         item = temp;
@@ -35,10 +42,9 @@ public class Infinidirt extends Relic {
     public void Activate(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         Random random = new Random();
-        if(event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-        //Change for how many switch cases you have.
-        event.getItem().setAmount(1);
-    }
+        if(event.getAction() == Action.PHYSICAL) {
+            event.getItem().setAmount(0);
+        }
     }
     
 }
