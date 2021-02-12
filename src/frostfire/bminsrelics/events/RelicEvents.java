9e1e8b8a39879505b80f0411b108fd1f92ab5774
Brendonk <bminor230@aim.com>
2021-02-11 20:19:31 -0600

    Lightning Hammer and random fixes

--- src/frostfire/bminsrelics/events/RelicEvents.java
+++ src/frostfire/bminsrelics/events/RelicEvents.java
@@ -1,9 +1,7 @@
 package frostfire.bminsrelics.events;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import frostfire.bminsrelics.item.ItemManager;
