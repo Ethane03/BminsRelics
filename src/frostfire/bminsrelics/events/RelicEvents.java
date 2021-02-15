12163a58c1e37b3ce146faacdc613586dc5d4b42
Ethane03 <ethan03@tjfmail.com>
2021-02-14 18:52:38 -0600

    fixed events for both trick sword and bow

--- src/frostfire/bminsrelics/events/RelicEvents.java
+++ src/frostfire/bminsrelics/events/RelicEvents.java
@@ -1,7 +1,9 @@
 package frostfire.bminsrelics.events;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
@@ -20,9 +22,19 @@ public class RelicEvents implements Listener {
             }
         }
     }
+    @EventHandler
     public static void onShoot(EntityShootBowEvent event){
         if(event.getBow() != null) {
             Relic relic = ItemManager.GetRelic(event.getBow());
+            if(relic!=null) {
+                relic.Activate(event);
+            }
+        }
+    }
+    @EventHandler
+    public static void onHit(EntityDamageByEntityEvent event){
+        if(((Player)event.getDamager()).getInventory().getItemInMainHand() != null) {
+            Relic relic = ItemManager.GetRelic(((Player)event.getDamager()).getInventory().getItemInMainHand());
             if(relic!=null) {
                 relic.Activate(event);
             }
