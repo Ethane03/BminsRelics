6bcc9a1714442306b77008d9b37ac3cff76670b6
Brendonk <bminor230@aim.com>
2021-03-01 21:51:22 -0600

    FixFix. No duplicate events!

--- src/frostfire/bminsrelics/events/RelicEvents.java
+++ src/frostfire/bminsrelics/events/RelicEvents.java
@@ -22,18 +22,6 @@ import java.util.logging.Logger;
 
 public class RelicEvents implements Listener {
     @EventHandler
-    static void shootProjectile(ProjectileHitEvent event){
-        Bukkit.broadcastMessage("projectile has been shoot");
-        if(event.getEntity().getShooter() instanceof Player) {
-            Bukkit.broadcastMessage("Shooter was a player");
-            Relic relic = ItemManager.GetRelic(event.getEntity().getName());
-            if(relic!=null) {
-                Bukkit.broadcastMessage("Projectile was a relic");
-                relic.Activate(event);
-            }
-        }
-    }
-    @EventHandler
     public static void onRightClick(PlayerInteractEvent event) {
         if(event.getItem() != null) {
             Relic relic = ItemManager.GetRelic(event.getItem());
